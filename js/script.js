bafac1da08f7500ab9cadb4f8ed8211a4e9fe3bc
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-26 11:51:23 -0800

    create a ticket constructor
    Co-authored-by: Logan Roth <diamondintheroth@gmail.com>
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let movies = ["Skooby Doo", "Batman", "Iron man", "Avengers", "I am legend", "Titanic", "Alladin", "Wonder Woman"]
+
+function Ticket(name, time, age) {
+  this.name = name;
+  this.time = time;
+  this.age = age;
+  this.price = 10;
+}
+
+function changePrice(ticket) {
+
+}
